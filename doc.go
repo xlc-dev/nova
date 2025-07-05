3a58f024b541ae4e61a8ad242a3bb86b03a5dd8d
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2025-07-05 19:23:29 +0200

    add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package main implements a Fullstack HTTP Web Framework called Nova.
+
+See https://xlc-dev.github.io/nova for more information about Nova.
+*/
+package main // import "github.com/xlc-dev/nova/nova"
